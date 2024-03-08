becdec3b85d6e7e97397388cf3df468616ad2af4
Bendy545 <chmelik@spsejecna.cz>
2024-03-08 17:19:08 +0100

    Added constructor and ToString into Farmer class and in Flower class added an constructor aswell

--- src/Farmer.java
+++ src/Farmer.java
@@ -2,4 +2,17 @@ public class Farmer {
 
     private String name;
     private int age;
+
+    public Farmer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Farmer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
